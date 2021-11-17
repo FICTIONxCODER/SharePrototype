2ab4e1f0561c4c820994aee2770858a6b1b88dc8
Abhishek Chaudhary <acha@bea.be>
2021-11-17 19:54:48 +0530

    File name added with UTC date and time

--- app/src/main/java/com/bea/shareprototype/MainActivity.kt
+++ app/src/main/java/com/bea/shareprototype/MainActivity.kt
@@ -2,14 +2,17 @@ package com.bea.shareprototype
 
 import android.Manifest
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
 import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.io.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var fileReader:FileReader
@@ -32,10 +35,15 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1);
         }
 
-
-
-        var saveButton:View = findViewById<View>(R.id.BtnShare)
-        var file:File = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),"myJSONfile")
+        var saveButton:View = findViewById(R.id.BtnSave)
+        var readButton:View = findViewById(R.id.BtnRead)
+        var ReadTextView:TextView = findViewById(R.id.ReadText)
+        val dateFormatGmt = SimpleDateFormat("dd:MM:yyyyHH:mm:ss")
+        dateFormatGmt.setTimeZone(TimeZone.getTimeZone("GMT"))
+        var fileDate = dateFormatGmt.format(Date()).toString() + ""
+        var file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+            "WS_SensorCANNumber$fileDate.json"
+        )
 
         saveButton.setOnClickListener {
             if (!file.exists()) {
@@ -43,7 +51,7 @@ class MainActivity : AppCompatActivity() {
                     file.createNewFile()
                     var fileWriter = FileWriter(file.absoluteFile)
                     bufferWriter = BufferedWriter(fileWriter)
-                    bufferWriter.write("{}")
+                    bufferWriter.write("{Hello world}")
                     bufferWriter.close()
                     Log.i("New File",file.name + file.absolutePath )
                 } catch (e: IOException) {
@@ -51,7 +59,20 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             Log.i("File Exist",file.name + file.absolutePath )
+        }
 
+        readButton.setOnClickListener {
+            var output:StringBuffer = StringBuffer()
+            fileReader = FileReader(file.absoluteFile)
+            bufferReader = BufferedReader(fileReader)
+            var line:String = ""
+            while ((bufferReader.readLine()) != null){
+                output.append(line+"\n")
+                }
+            response = output.toString()
+            Log.i("Read",response!!)
+            ReadTextView.text = response!!
+            bufferReader.close()
         }
     }
 }
