fde6f75aaca7ccd3e59ae0c9fb5f377e45df5cc0
Abhishek Chaudhary <acha@bea.be>
2021-12-07 15:02:26 +0530

    Putting list of files in recycler View

--- app/src/main/java/com/bea/shareprototype/MainActivity.kt
+++ app/src/main/java/com/bea/shareprototype/MainActivity.kt
@@ -10,12 +10,15 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import java.io.*
 import java.nio.MappedByteBuffer
 import java.nio.channels.FileChannel
 import java.nio.charset.Charset
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bufferWriter:BufferedWriter
@@ -25,9 +28,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+
+
+
         var saveButton:View = findViewById(R.id.BtnSave)
         var readButton:View = findViewById(R.id.BtnRead)
-        var ReadTextView:TextView = findViewById(R.id.ReadText)
         val dateFormatGmt = SimpleDateFormat("dd:MM:yyyyHH:mm:ss")
         dateFormatGmt.setTimeZone(TimeZone.getTimeZone("GMT"))
         var fileDate = dateFormatGmt.format(Date()).toString() + ""
@@ -68,8 +73,47 @@ class MainActivity : AppCompatActivity() {
                 val bb: MappedByteBuffer = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size())
                 /* Instead of using default, pass in a decoder. */
                  jString = Charset.defaultCharset().decode(bb).toString()
-                 ReadTextView.text = jString!!
+
                 Log.i("Reading", jString)
+
+                //Listing files
+                val path = Environment.getExternalStorageDirectory().toString() + "/Download"
+                Log.d("Files", "Path: $path")
+                val directory = File(path)
+                val files = directory.listFiles()
+                // ArrayList of class ItemsViewModel
+                val data = ArrayList<MyListData>()
+                if(files != null)
+                {
+                    Log.d("Files", "Size: " + files.size)
+                    for (i in files.indices) {
+                        Log.d("Files", "FileName:" + files[i].name)
+                        data.add(MyListData(R.drawable.ic_baseline_article_24, files[i].name))
+                    }
+                }
+                else
+                    Log.d("Files", "Files are not present")
+
+                // getting the recyclerview by its id
+                val recyclerview = findViewById<RecyclerView>(R.id.RecyclerViewFiles)
+
+                // this creates a vertical layout Manager
+                recyclerview.layoutManager = LinearLayoutManager(this)
+
+
+
+                /*// This loop will create 20 Views containing
+                // the image with the count of view
+                for (i in 1..20) {
+                    data.add(MyListData(R.drawable.ic_baseline_article_24, "Item " + i))
+                }*/
+
+                // This will pass the ArrayList to our Adapter
+                val adapter = MyListAdapter(data)
+
+                // Setting the Adapter with the recyclerview
+                recyclerview.adapter = adapter
+
             } finally {
                 stream.close()
             }
